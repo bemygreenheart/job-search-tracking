6332c53830b0cfcc852edbe2d8a88375bdd5bb0a
Javokhirbek Nazarov <javohirnazarov99@gmail.com>
2022-08-10 00:10:12 -0500

    add mapper update method and implement service methods

--- src/main/java/uz/jaxathon/jobsearchtracking/mappers/BaseMapper.java
+++ src/main/java/uz/jaxathon/jobsearchtracking/mappers/BaseMapper.java
@@ -1,6 +1,11 @@
 package uz.jaxathon.jobsearchtracking.mappers;
 
+import org.mapstruct.MappingTarget;
+
 public interface BaseMapper<E, D> {
     E mapDtoToEntity(D dto);
     D mapEntityToDto(E entity);
+
+    void updateEntityFromDto(D dto, @MappingTarget E entity);
+
 }
--- src/main/java/uz/jaxathon/jobsearchtracking/services/JobOpeningService.java
+++ src/main/java/uz/jaxathon/jobsearchtracking/services/JobOpeningService.java
@@ -4,24 +4,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import uz.jaxathon.jobsearchtracking.dto.JobOpeningDto;
 import uz.jaxathon.jobsearchtracking.entities.JobOpening;
+import uz.jaxathon.jobsearchtracking.exceptions.ResourceNotFoundException;
+import uz.jaxathon.jobsearchtracking.mappers.JobOpeningMapper;
 import uz.jaxathon.jobsearchtracking.repos.JobOpeningRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobOpeningService {
 
     private final JobOpeningRepository repository;
+    private final JobOpeningMapper mapper;
 
     @Autowired
-    public JobOpeningService(JobOpeningRepository repository) {
+    public JobOpeningService(JobOpeningRepository repository, JobOpeningMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     public Page<JobOpening> getAllOpenings(Pageable page) {
         return repository.findAll(page);
     }
 
+    public JobOpening createJobOpening(JobOpeningDto dto){
+        JobOpening entity = mapper.mapDtoToEntity(dto);
+        return repository.save(entity);
+    }
+
+    public JobOpening getById(Long id){
+        return getOrThrow404(id);
+    }
+
+    public void delete(Long id){
+        JobOpening opening = getOrThrow404(id);
+        repository.delete(opening);
+    }
+
+    public JobOpening update(Long id, JobOpeningDto dto){
+        JobOpening opening = getOrThrow404(id);
+        mapper.updateEntityFromDto(dto, opening);
+        return repository.save(opening);
+    }
+
+    private JobOpening getOrThrow404(Long id){
+        Optional<JobOpening> jobOpening = repository.findById(id);
+        if(jobOpening.isPresent()){
+            return jobOpening.get();
+        }else{
+            throw new ResourceNotFoundException("Job opening with given id [ " + id + " ] does not exist");
+        }
+    }
 
 }
